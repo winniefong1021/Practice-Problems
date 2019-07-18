e7c1b7f1c47781dc58f7c4c88c9c85e29eb36c70
Winnie Fong <winniefong21@gmail.com>
2019-07-18 16:15:33 -0700

    Set up prompt

---
+++ areThereDuplicates.js
@@ -0,0 +1,20 @@
+/*
+Implement a function called, areThereDuplicates which accepts a variable number of arguments,
+and checks whether there are any duplicates among the arguments passed in.
+
+Restrictions:
+Time - O(n)
+Space = O(n)
+
+Bonus:
+Time - O(n log n)
+Space - O(1)
+
+areThereDuplicates(1, 2, 3); // false
+areThereDuplicates(1, 2, 2); // true
+areThereDuplicates('a', 'b', 'c', 'a', 'c'); // true
+*/
+
+function areThereDuplicates() {
+
+}
