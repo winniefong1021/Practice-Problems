72fffe67ee88b843fe6c8a0416258102236f42a7
Winnie Fong <winniefong21@gmail.com>
2019-07-20 11:35:59 -0700

    Set up prompt

---
+++ isSubsequence.js
@@ -0,0 +1,15 @@
+/*
+Write a function called, isSubsequence, that takes in two strings and checks whether the characters in the first string form
+a subsequence of the characters in the second string.
+
+Time - O(n + m)
+Space - O(1)
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'acb'); // false
+*/
+
+function isSubsequence(str1, str2) {
+
+}
