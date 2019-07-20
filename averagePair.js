797ee8a39b7da85975662bad2899c2b33e74c315
Winnie Fong <winniefong21@gmail.com>
2019-07-19 17:13:17 -0700

    Solve with O(n) time and O(1) space

--- averagePair.js
+++ averagePair.js
@@ -14,5 +14,19 @@ averagePair([], 4); // false
 */
 
 function averagePair(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
 
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2;
+    if (avg === target) {
+      return true;
+    } else if (avg < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return false;
 }
