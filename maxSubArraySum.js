a1f17df1c115f2ad2c713c65745c2c7ba7888218
Winnie Fong <winniefong21@gmail.com>
2019-07-22 14:59:58 -0700

    Set up prompt

---
+++ maxSubArraySum.js
@@ -0,0 +1,19 @@
+/*
+Given an array of integers and a number, write a function called maxSubArraySum, which finds the max sum of a subarray
+with the length of the number passed in.
+
+Note that the subarray must consist of consecutive elements from the original array.
+
+Constraints:
+Time - O(n)
+Space - O(1)
+
+maxSubArraySum([100, 200, 300, 400], 2); // 700
+maxSubArraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); // 39
+maxSubArraySum([-3, 4, 0, -2, 6, -1], 2); // 5
+maxSubArraySum([2, 3], 3); // null
+*/
+
+function maxSubArraySum(arr, lgth) {
+
+}
