a1be3f5b3f30a2d8493a102745c5467ca565d192
Winnie Fong <winniefong21@gmail.com>
2019-07-22 14:48:00 -0700

    Solve isSubSeq using multiple pointers

--- isSubsequence.js
+++ isSubsequence.js
@@ -8,8 +8,28 @@ Space - O(1)
 isSubsequence('hello', 'hello world'); // true
 isSubsequence('sing', 'sting'); // true
 isSubsequence('abc', 'acb'); // false
+isSubsequence('', 'acb'); // true
 */
 
 function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
 
+  if (str1.length === 0) {
+    return true;
+  }
+
+  while (j < str2.length) {
+    if (str1[i] === str2[j]) {
+      i++;
+    }
+
+    if (i === str1.length) {
+      return true;
+    }
+
+    j++;
+  }
+
+  return false;
 }
