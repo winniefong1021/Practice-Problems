e240a849b10dd106e06383c03cf74859abca2bd9
Winnie Fong <winniefong21@gmail.com>
2019-07-23 13:41:47 -0700

    Set up prompt

---
+++ findLongestSubString.js
@@ -0,0 +1,15 @@
+/*
+Write a function called findLongestSubString which accepts a string and returns the length of the longest
+substring with all distinct characters.
+
+Time Complexity: O(n)
+
+findLongestSubString(''); // 0
+findLongestSubString('thisishowwedoit'); // 6
+findLongestSubString('bbbbb'); // 1
+findLongestSubString('thecatinthehat'); // 7
+*/
+
+function findLongestSubString(str) {
+
+}
