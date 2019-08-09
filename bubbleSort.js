11aab58e0fb1ebe9f7a6ad54667ecd4064d9ac7b
Winnie Fong <winniefong21@gmail.com>
2019-08-09 14:07:50 -0700

    Optimize by cutting down swaps

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,13 +1,17 @@
 const bubbleSort = (arr) => {
+  let noSwap;
   for (let i = arr.length; i > 0; i--) {
+    noSwap = true;
     for (let j = 0; j < i - 1; j++) {
       console.log(arr, arr[j], arr[j+1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSwap = false;
       }
     }
+    if (noSwap) break;
   }
   return arr;
 }
