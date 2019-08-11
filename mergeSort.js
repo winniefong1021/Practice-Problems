04e7ae88ea32d0ee418983aab260699654c2778e
Winnie Fong <winniefong21@gmail.com>
2019-08-10 17:23:30 -0700

    Implement merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -27,7 +27,11 @@ const merge = (arr1, arr2) => {
 }
 
 const mergeSort = (arr) => {
-
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
 }
 
-mergeSort([8, 3, 5, 4, 7, 6, 1, 2]);
+console.log(mergeSort([8, 3, 5, 4, 7, 6, 1, 2]));
