8e6f13b56bbf55c32c50554f6bcc7870adf1fc3d
Winnie Fong <winniefong21@gmail.com>
2019-08-10 17:02:41 -0700

    Implement merge function to combine sorted arrs

---
+++ mergeSort.js
@@ -0,0 +1,33 @@
+const merge = (arr1, arr2) => {
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
+  return results;
+}
+
+const mergeSort = (arr) => {
+
+}
+
+mergeSort([8, 3, 5, 4, 7, 6, 1, 2]);
