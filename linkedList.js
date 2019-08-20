fd2035c891c8b06024164aa1742eac01972132ce
Winnie Fong <winniefong21@gmail.com>
2019-08-20 16:41:35 -0700

    Implement push method

--- linkedList.js
+++ linkedList.js
@@ -5,10 +5,31 @@ class Node {
   }
 }
 
-class linkedList {
+class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
 }
+
+let list = new LinkedList();
+list.push(1);
+list.push(4);
+list.push(6);
+console.log(list);
