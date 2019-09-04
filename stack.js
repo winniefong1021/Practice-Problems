cd460a610432c7c20e3616fc2eebac1f7be02805
Winnie Fong <winniefong21@gmail.com>
2019-09-04 13:42:03 -0700

    Implement pop method

--- stack.js
+++ stack.js
@@ -29,11 +29,27 @@ class Stack {
     this.size++;
   }
 
+  // remove node from beginning of stack - O(1) constant
+  pop() {
+    let current = this.first;
+    if (this.size === 0) return null;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    }
+    this.first = current.next;
+    this.size--;
+    return current;
+  }
+
 }
 
 let stackList = new Stack();
 stackList.push(1);
 stackList.push(2);
 stackList.push(3);
+stackList.pop();
+stackList.pop();
+stackList.pop();
 console.log(stackList);
 
