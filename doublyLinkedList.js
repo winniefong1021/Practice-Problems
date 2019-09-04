9f98ebffc478ef20787249806a7010b264131ca1
Winnie Fong <winniefong21@gmail.com>
2019-09-04 10:30:05 -0700

    Implement reverse method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -162,6 +162,23 @@ class DoublyLinkedList {
     this.length--;
     return removeNode;
   }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+
+    return this;
+  }
 }
 
 let list = new DoublyLinkedList();
