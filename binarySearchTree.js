e9f9d46a808ec0070ced67dd1d83dfbf91651d83
Winnie Fong <winniefong21@gmail.com>
2019-09-05 20:24:52 -0700

    Set up binary search tree and node class

---
+++ binarySearchTree.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(40);
+console.log(tree);
