9576a3eb5cc93397c5608eb434bf2ed8b30d5914
Winnie Fong <winniefong21@gmail.com>
2019-09-06 15:21:49 -0700

    Implement find method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -24,14 +24,14 @@ class BinarySearchTree {
 
       if (current.val === newNode.val) return undefined;
 
-      if (newNode.val > current.val) { // check right side if greater
+      if (newNode.val > current.val) {
         if (!current.right) {
           current.right = newNode;
           return this;
         }
         current = node.right;
         checkNodeVal(current);
-      } else { // check left side if less
+      } else {
         if (!current.left) {
           current.left = newNode;
           return this;
@@ -43,9 +43,37 @@ class BinarySearchTree {
 
     checkNodeVal(this.root);
   }
+
+  find(val) {
+    if (!this.root) return false;
+
+    let found = false;
+    let findNode = (node) => {
+      let current = node;
+
+      if (current.val === val) {
+        found = true;
+        return found;
+      }
+
+      if (val > current.val) {
+        if (!current.right) return found;
+        current = node.right;
+        findNode(current);
+      } else {
+        if (!current.left) return found;
+        current = node.left;
+        findNode(current);
+      }
+    }
+
+    findNode(this.root);
+    return found;
+  }
 }
 
 let tree = new BinarySearchTree();
+tree.find(40); // false
 tree.root = new Node(40);
 tree.insert(20); // root: 40, left: 20
 tree.insert(22); // root: 40, left: 20, 20.right: 22
@@ -55,4 +83,7 @@ tree.insert(50); // root: 40, right: 50
 tree.insert(44); // root: 40, right: 50, 50.left: 44
 tree.insert(55); // root: 40, right: 50, 50.right: 55
 tree.insert(55); // undefined
+tree.find(40); // true
+tree.find(22); // true
+tree.find(67); // false
 console.log(tree);
