e8adcc68f1422d6df35ef06663fcb08fb438377f
Winnie Fong <winniefong21@gmail.com>
2019-09-05 17:43:26 -0700

    Implement enqueue method

--- queue.js
+++ queue.js
@@ -11,7 +11,25 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(val) {
+    let newNode = new Node(val);
+
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+
+    let oldTail = this.last;
+    oldTail.next = newNode;
+    this.last = newNode;
+    this.size++;
+  }
 }
 
 let queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
 console.log(queue);
